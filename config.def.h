53c5bdb4ea3a65447be7d4e4259d6db2bf86a73c
Diego Peña y Lillo <80992641+Diegopyl1209@users.noreply.github.com>
2025-08-13 16:11:58 -0400

    pertag patch

--- config.def.h
+++ config.def.h
@@ -31,6 +31,8 @@ static uint32_t colors[][3]                = {
 /* tagging - TAGCOUNT must be no greater than 31 */
 static char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
+#define TAGCOUNT (sizeof(tags) / sizeof(tags[0]))
+
 /* logging */
 static int log_level = WLR_ERROR;
 
